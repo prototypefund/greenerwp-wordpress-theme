127ed613b5b393b8c4e80938871ee5f8e86c22ab
Christian Neumann <cneumann@e3code.de>
2020-01-31 14:21:22 +0100

    Add PHPCS ignore.

--- functions.php
+++ functions.php
@@ -79,7 +79,7 @@ add_action( 'after_setup_theme', 'lowtechwp_zero_setup' );
 function lowtechwp_zero_content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+	// phpcs:ignore WPThemeReview.CoreFunctionality.PrefixAllGlobals.NonPrefixedVariableFound
 	$GLOBALS['content_width'] = apply_filters( 'lowtechwp_zero_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'lowtechwp_zero_content_width', 0 );
